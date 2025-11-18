04969646ad4369b3d5456265e55fd385ca24d3bc
Caleb-J-Hill <calebhill11@gmail.com>
2025-11-18 00:01:34 +0000

    Deploying to gh-pages from @ Caleb-J-Hill/Caleb-J-Hill.github.io@b528196b7a7ebe2cf3c0d28a159cef479b163045 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -422,9 +422,9 @@ ninja.data = [{
           description: "another without an image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-",
-          title: '',
-          description: "",
+            },},{id: "projects-asymmetric-contests-with-investment-an-experimental-investigation",
+          title: 'Asymmetric Contests with Investment: An Experimental Investigation',
+          description: "a project with a background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
